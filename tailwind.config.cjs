0b7183bf350d924c6dfb71a7dfd1cf9da9dd4594
Robin Lau <Robi0279@stud.kea.dk>
2022-12-12 10:25:27 +0100

    farver

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      white: "#FFFEFE",
+      black: "#200F00",
+      'grey-light': "#F2F2F2",
+      'grey-dark': "#7F7F7F",
+      'orange-brand': "#FEA255",
+      'orange-light': "#FFF6EE",
+      yellow: "#FFCE61",
+      green: "#2FCC8C",
+    },
+
+    fontsize: {
+      
+    }
     extend: {},
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
